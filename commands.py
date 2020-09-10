e84b08b51f568e41fcf7c5ade825b23716ffd8f4
a-turko <andrzej.turko@gmail.com>
2020-09-10 22:05:17 +0200

    Implemented tools for command recognition

--- commands.py
+++ commands.py
@@ -1,32 +1,63 @@
 
 import debugtools as dbg
+import re
 
 # definitions of command classes
 # module for command recognition from natural language will be added here
 
 # class for representing an argument
 class ARG:
-	def __init__(self, name, indicators, validate, read):
+	def __init__(self, name, validate, read):
 		self.name = name
-		self.indicators = indicators	# indicators for command recognition
 		self.validate = validate		# procedure for checking whether the value is valid
 		self.read = read				# read values of the arguments from tokens, takes list of tokens as argument
 										# returns the number of tokens read
+		self.hasRecognitionData = False # true iff the instance has valueAliases and indicators defined
+
+	# valueAliases is a list of procedures for translating
+	# possible values to standard ones (as ARG.read would return them)
+	# for example tonight in stead of specific UNIX timestamp
+	# those procedures return None on failure
+	def setValueAliases(self, aliases):
+		self.valueAliases = [self.read] + aliases
+	
+	# possible indicators preceding the value of for this argument
+	def setIndicators(self, indicators):
+		self.indicators = set(indicators)
+
+	# returns a list of possible values for the argument
+	def scanForValues(self, tokens):
+		if not self.hasRecognitionData:
+			return []
+		
+		ret = []
+		ln = len(tokens)
+		for i in range(ln):
+			if not tokens[i] in self.indicators: continue
+			
+			for proc in self.valueAliases:
+				val = proc(tokens, i+1)
+				if not val is None and self.validate(val):
+					ret.append(val)
+			
+		return ret
+
+
+		
 
 	# creates a read procedure for a list of values with types specified by valTypes
-	# returns a list of values
+	# returns a list of values on success and None on failure
 	@staticmethod
 	def makeReader(valTypes):
 		def reader(values, start):
+			if start + len(valTypes) > len(values)):
+				return None
+			
 			ret = []
-			assert(start + len(valTypes) <= len(values))
 			for i,valType in enumerate(valTypes):
-				if valType=="int":
-					ret.append(int(values[start+i]))
-				if valType=="float":
-					ret.append(float(values[start+i]))
-				if valType=="string":
-					ret.append(str(values[start+i]))
+				if valType=="int": ret.append(int(values[start+i]))
+				if valType=="float": ret.append(float(values[start+i]))
+				if valType=="string": ret.append(str(values[start+i]))
 			
 			return ret
 		
@@ -56,16 +87,6 @@ class ARG:
 
 # class for representing a command
 class CMD:
-	
-	# A default command recognition procedure
-	# try to recognize the command from the line, returns:
-	# a dictionary with recognized arguments in case of success
-	# None in case of failure
-	# TODO: implement
-	@staticmethod
-	def recognition(self, line):
-		return None
-
 
 	# creates a simple procedure for validation, which checks
 	# whether all the arguments from argList are present in ArgDict
@@ -77,10 +98,7 @@ class CMD:
 			return True
 		return validator
 
-	def __init__(self, name, arguments, validate, execute, recognize = None):
-		
-		# default values:
-		if recognize is None: recognize = CMD.recognition
+	def __init__(self, name, arguments, validate, execute, customRecognize = None):
 
 		self.name = name
 		self.arguments = arguments		# dictionary with ARG objects by name
@@ -90,8 +108,18 @@ class CMD:
 		self.execute = execute			# procedure for executing a command, takes a Session object as an argument
 										# execute procedure takes a Session object and a dictionary with arguments
 
-		self.recognize =  recognize		# recognizes the command and its arguments from natural language, returns
-										# dictionary with arguments in case of success and None in case of failure
+
+		self.customRecognize =  customRecognize		# a custom procedure for recognition of this procedure, see recognize
+
+	# keywords is a list of keywords by which the command can be recognized
+	# self.keywords stores the keywords in a set
+	def setKeywords(self, keywords):
+		self.keywords = set(keywords)
+	
+	# set the autofill procedure which takes only one argument
+	# the argict and possibly modyfies it by filling in default argument values
+	def setAutofill(autofill):
+		self.autofill = autofill
 
 	# checks whether the token is a name of a option proceeded by '--'
 	# if it's not returns None, and right ARG otherwise
@@ -123,6 +151,7 @@ class CMD:
 			
 			
 			vals = argument.read(tokens, i)
+			assert(not vals is None)
 			i+=len(vals)
 
 
@@ -132,7 +161,33 @@ class CMD:
 		return argumentDict
 	
 
-	
+	# a procedure for recognizing the command from natural language (english)
+	# on success returns values of arguments stored in a dictionary,
+	# on failure returns None
+	# For now operates on limited set of keywords
+	def regonize(self, line):
+		tokens = re.split(" |?|,|.", line.lower())
+		keywordFound = False
+		for token in tokens:
+			if token in keywords: keywordFound = True
+		
+		if not keywordFound: return None
+
+		argDict = dict()
+
+		for argName in self.arguments:
+			arg = self.arguments[argName]
+
+			pVals = arg.scanForValues
+			if len(pVals)==0: continue
+
+			# TODO: change this temporary solution
+			argDict[argName] = pVals[0]
+		
+		if self.validate(argDict): 
+			return argDict
+		else:
+			return None
 
 	
 	
